98219058caffde21c2f7685101b98aaddbe9099a
Jamon Bailey <infinity.dev@outlook.com>
2025-05-25 09:30:02 -0400

    Updated info.h

---
+++ firmware/meta/info.h
@@ -0,0 +1,48 @@
+
+/*!
+* @file info.h
+*
+* @brief
+* Firmware identification document.
+*/
+
+
+#ifndef DEMO_FIRMWARE_IDENTITY_H_
+#define DEMO_FIRMWARE_IDENTITY_H_
+
+namespace DemoFirmwareMeta
+{
+
+    /*!
+    * @brief
+    * Firmware publisher.
+    */
+    const char* PUBLISHER{ "N/A" };
+
+    /*!
+    * @brief
+    * Firmware universally unique identifier.
+    */
+    const char* UUID{ "N/A" };
+
+    /*!
+    * @brief
+    * Firmware distribution license type.
+    */
+    const char* LICENSE_TYPE{ "N/A" };
+
+    /*!
+    * @brief
+    * Firmware full name.
+    */
+    const char* OFFICIAL_NAME{ "Demo Firmware" };
+
+    /*!
+    * @brief
+    * Firmware shortend name.
+    */
+    const char* SHORT_NAME{ "Demo" };
+
+}
+
+#endif // DEMO_FIRMWARE_IDENTITY_H_
