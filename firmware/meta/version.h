2e987b95f9641d2e3decf7c716075b77e53360f7
Jamon Bailey <infinity.dev@outlook.com>
2025-05-25 09:30:16 -0400

    Updated version.h

---
+++ firmware/meta/version.h
@@ -0,0 +1,42 @@
+
+/*!
+* @file version.h
+*
+* @brief
+* Firmware version document.
+*/
+
+
+#ifndef DEMO_FIRMWARE_VERSION_H_
+#define DEMO_FIRMWARE_VERSION_H_
+
+namespace DemoFirmwareMeta
+{
+
+    /*!
+    * @brief
+    * Firmware version major.
+    */
+    const unsigned long int VERSION_MAJOR = 0;
+
+    /*!
+    * @brief
+    * Firmware version minor.
+    */
+    const unsigned long int VERSION_MINOR = 0;
+
+    /*!
+    * @brief
+    * Firmware version patch number.
+    */
+    const unsigned long int VERSION_PATCH = 0;
+
+    /*!
+    * @brief
+    * Firmware version tweak number.
+    */
+    const unsigned long int VERSION_TWEAK = 0;
+
+}
+
+#endif // DEMO_FIRMWARE_VERSION_H_
